0c8abaf6cf5c5c4deb603447164991a8386c68b0
Your Name <you@example.com>
2023-03-05 22:19:39 +0300

    updated

--- barbers/urls.py
+++ barbers/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("appointment", views.appointment, name="appointment"),
     path("register", views.register_view, name="register"),
-    path("login", views.login_view, name="login")
+    path("login", views.login_view, name="login"),
+    path('logout/',views.logoutUser,name="logout"),
 ]
--- barbers/views.py
+++ barbers/views.py
@@ -1,42 +1,16 @@
 from django.http import HttpResponse
 ## from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.shortcuts import render,redirect,reverse,get_object_or_404
 from barbers.models import Appointment
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
+#Login Requirements
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     return render(request, "barbers/index.html")
-def appointment(request):
-      # POST
-  if request.method == 'POST':
-      # Save order data to the database
-    add = Appointment(
-              fname=request.POST.get('fname'),
-              lname=request.POST.get('lname'),
-              phone=request.POST.get('phone'),
-              email=request.POST.get('email'),
-              contact=request.POST.get('contact'),
-              barber=request.POST.get('barber'),
-              date=request.POST.get('myDate'),
-              time=request.POST.get('time'),
-              comment=request.POST.get('comment'),
-            )
 
-    add.save()
-
-    if add is not None:
-     context = {
-      "client": Appointment.objects.last(),
-     }
-     return render(request, "barbers/index.html", context)
-    else:
-      return HttpResponse('{"success": false, "message": "Invalid input"}')
-
- # GET
-  else:
-    return render(request, "barbers/appointment.html")
 
 def login_view(request):
   # POST
@@ -94,3 +68,46 @@ def register_view(request):
     user = authenticate(request, username=username, password=password)
     login(request, user)
     return HttpResponse('{"success": true, "message": ""}')
+
+
+
+#it render the user to login page
+@login_required(login_url='login')    
+def appointment(request):
+      # POST
+  if request.method == 'POST':
+      # Save order data to the database
+    add = Appointment(
+              fname=request.POST.get('fname'),
+              lname=request.POST.get('lname'),
+              phone=request.POST.get('phone'),
+              email=request.POST.get('email'),
+              contact=request.POST.get('contact'),
+              barber=request.POST.get('barber'),
+              date=request.POST.get('myDate'),
+              time=request.POST.get('time'),
+              comment=request.POST.get('comment'),
+            )
+
+    add.save()
+
+    if add is not None:
+     context = {
+      "client": Appointment.objects.last(),
+     }
+     return render(request, "barbers/index.html", context)
+    else:
+      return HttpResponse('{"success": false, "message": "Invalid input"}')
+
+ # GET
+  else:
+    return render(request, "barbers/appointment.html")
+
+
+
+
+
+#LOGOUT VIEWS
+def logoutUser(request):
+    logout(request)
+    return redirect('/')
